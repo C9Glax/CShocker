3bbde48796fbb0bfbb7425fe1454dd5e65f34aef
glax <johanna@bernloehr.eu>
2024-01-17 04:31:28 +0100

    Fix abstract httpShockersettings

--- CShocker/Shockers/ShockerSettings/HttpShockerSettings.cs
+++ CShocker/Shockers/ShockerSettings/HttpShockerSettings.cs
@@ -3,7 +3,7 @@ using CShocker.Shockers.ShockerSettings.Abstract;
 
 namespace CShocker.Shockers.ShockerSettings;
 
-public abstract record HttpShockerSettings(string[] ShockerIds, IntensityRange Intensity, DurationRange Duration, string ApiKey, string Endpoint) : AShockerSettings(ShockerIds, Intensity, Duration)
+public record HttpShockerSettings(string[] ShockerIds, IntensityRange Intensity, DurationRange Duration, string ApiKey, string Endpoint) : AShockerSettings(ShockerIds, Intensity, Duration)
 {
     internal readonly HttpClient HttpClient = new ();
     internal readonly string ApiKey = ApiKey, Endpoint = Endpoint;
