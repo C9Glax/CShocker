076b6fc271e6bb31e2ffdc3eebb9dbfe95cac057
glax <johanna@bernloehr.eu>
2024-01-20 20:02:12 +0100

    Add TestApp

--- CShocker/Shockers/APIS/PiShockSerial.cs
+++ CShocker/Shockers/APIS/PiShockSerial.cs
@@ -6,13 +6,34 @@ namespace CShocker.Shockers.APIS;
 
 public class PiShockSerial : SerialShocker
 {
-    public PiShockSerial(List<string> shockerIds, IntensityRange intensityRange, DurationRange durationRange, ILogger? logger = null) : base(shockerIds, intensityRange, durationRange, ShockerApi.PiShockSerial, logger)
+    private const int BaudRate = 115200;
+    public PiShockSerial(List<string> shockerIds, IntensityRange intensityRange, DurationRange durationRange, SerialPortInfo serialPortI, ILogger? logger = null) : base(shockerIds, intensityRange, durationRange, serialPortI, BaudRate, ShockerApi.PiShockSerial, logger)
     {
         throw new NotImplementedException();
     }
 
     protected override void ControlInternal(ControlAction action, string shockerId, int intensity, int duration)
     {
-        throw new NotImplementedException();
+        string json = "{" +
+                      "\"cmd\": \"operate\"," +
+                      "\"value\":{" +
+                        $"\"op\": \"{ControlActionToOp(action)}\"," +
+                        $"\"duration\": {duration}," +
+                        $"\"intensity\": {intensity}," +
+                        $"\"id\": " +
+                        "}" +
+                      "}";
+        serialPort.WriteLine(json);
+    }
+
+    private static string ControlActionToOp(ControlAction action)
+    {
+        return action switch
+        {
+            ControlAction.Beep => "",
+            ControlAction.Vibrate => "vibrate",
+            ControlAction.Shock => "",
+            _ => ""
+        };
     }
 }
---
+++ TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using CShocker.Ranges;
+using CShocker.Shockers.Abstract;
+using CShocker.Shockers.APIS;
+using Microsoft.Extensions.Logging;
+
+Logger logger = new (LogLevel.Trace);
+
+#pragma warning disable CA1416
+List<SerialShocker.SerialPortInfo> serialPorts = SerialShocker.GetSerialPorts();
+
+if (serialPorts.Count < 1)
+    return;
+
+for(int i = 0; i < serialPorts.Count; i++)
+    Console.WriteLine($"{i}) {serialPorts[i]}");
+
+Console.WriteLine($"Select Serial Port [0-{serialPorts.Count-1}]:");
+string? selectedPortStr = Console.ReadLine();
+int selectedPort = -1;
+while (!int.TryParse(selectedPortStr, out selectedPort) || selectedPort < 0 || selectedPort > serialPorts.Count - 1)
+{
+    Console.WriteLine($"Select Serial Port [0-{serialPorts.Count-1}]:");
+    selectedPortStr = Console.ReadLine();
+}
+
+OpenShockSerial shockSerial = new (new Dictionary<string, OpenShockSerial.ShockerModel>(), new IntensityRange(30,50), new DurationRange(1000,1000), serialPorts[selectedPort], logger);
+Dictionary<string, OpenShockSerial.ShockerModel> shockers = shockSerial.GetShockers();
+Console.ReadKey();
